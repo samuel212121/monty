de8699d4dce419a2ae39d86123a3ccaae1503d79
samuel212121 <abemmenelik@gmail.com>
2024-03-30 08:15:20 -0700

    heder file

--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -14,7 +15,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO Alx  project
  */
 typedef struct stack_s
 {
@@ -44,7 +45,7 @@ extern bus_t bus;
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO Alx  project
  */
 typedef struct instruction_s
 {
